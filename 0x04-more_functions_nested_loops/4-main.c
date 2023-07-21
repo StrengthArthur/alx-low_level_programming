769622b9dac3642dc4fc494503876a6654f52504
StrengthArthur <arthurmillyb55@gmail.com>
2023-07-21 16:55:22 -0700

    May be of errors

---
+++ 0x04-more_functions_nested_loops/4-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - checks code
+ * print_most_numbers - prints numbers from 0 to 9 without 2 and 4
+ * Return: Always 0
+ */
+int main(void)
+
+void print_most_numbers(void);
+{
+	prints_most_numbers(0);
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+{
+
+char c
+
+	for (c = '0'; c <= '9'; c++)
+	if (c == '2')
+	if else(c == '4')
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
+
