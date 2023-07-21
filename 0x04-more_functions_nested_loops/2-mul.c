0926c5697f7811f01979598e35176fc28c5cf40c
StrengthArthur <arthurmillyb55@gmail.com>
2023-07-20 23:01:25 -0700

    Always

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - mltiplies two integers
+ * @a: integer to multiply
+ * @b: integer to multiply
+ * Return: Always 0
+ */
+int mul(int a, int b);
+{
+	_putchar('a * b');
+	_putchar('\n');
+	return (0);
+}
