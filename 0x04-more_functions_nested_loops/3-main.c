b651e4e972785b75df3045a3cda2b7ef922c1025
StrengthArthur <arthurmillyb55@gmail.com>
2023-07-21 09:05:23 -0700

    A function that prints numbers

---
+++ 0x04-more_functions_nested_loops/3-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - check the code
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 0; c <= 9; c++)
+	{
+	_putchar('c');
+	}
+	{
+	_putchar('\n');
+	}
+
+	return (0);
+}
+
