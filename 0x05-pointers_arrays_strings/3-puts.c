9a35cf70fffd399c3a6586d173af82cf85cb9b93
StrengthArthur <arthurmillyb55@gmail.com>
2023-07-24 19:15:06 -0700

    Printing a string in reverse

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *_puts - Prints a string
+ *@str: string to be printed
+ *Return: Always 0
+ */
+void _puts(char *str);
+{
+	char str = strength;
+
+	_putchar('%c');
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse
+ * @s: string to be printed in reverse
+ */
+void print_rev(char *s)
+{
+	int x;
+	int length = 0;
+	{
+		while (s[length] != '\0')
+	length++;
+	}
+	
+	for (x = length - 1; x >= 0; x--)
+	
+	{
+		_putchar(s[x]);
+	}
+	{
+		_putchar('\n');
+	}
+		
+}
