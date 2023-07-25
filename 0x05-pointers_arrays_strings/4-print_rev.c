27672dfd749125f911f28e38bb252519f15e7450
StrengthArthur <arthurmillyb55@gmail.com>
2023-07-24 19:20:14 -0700

    Coreections made to suit betty

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,17 +8,14 @@ void print_rev(char *s)
 	int x;
 	int length = 0;
 	{
-		while (s[length] != '\0')
+	while (s[length] != '\0')
 	length++;
 	}
-	
 	for (x = length - 1; x >= 0; x--)
-	
 	{
 		_putchar(s[x]);
 	}
 	{
 		_putchar('\n');
 	}
-		
 }
