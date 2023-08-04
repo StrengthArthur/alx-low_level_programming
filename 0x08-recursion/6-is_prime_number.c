44ff09c5680411e7473ca7f3a602049c05683762
StrengthArthur <arthurmillyb55@gmail.com>
2023-08-03 18:19:55 -0700

    Hard Things

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * is_prime_number - This function returns 1 if the input is a prime number.
+ * @n: integer
+ * Return: prime
+ */
+int is_prime_number(int n)
+{
+	return (prime(2, n));
+}
+/**
+ * prime - Returns if a number is primary or not
+ * @i: Value ++
+ * @n: Checks value
+ * Return: 1 and 0
+ */
+int prime(int i, int n)
+{
+	if (n <= 1)
+		return (0);
+	else if (i >= n)
+		return (1);
+	if (n % i != 0 && i != n)
+		return (prime(i + 1, n));
+	else
+		return (0);
+}
