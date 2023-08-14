0d9acb079ca9b94c1acd3cadc0b5bac335228cd0
StrengthArthur <arthurmillyb55@gmail.com>
2023-08-14 08:37:53 -0700

    Computing sum of x and y with function-like macro

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * A program that prints the name of the file compiled from
+ * _putchar - Prints a character
+ * Return: Always 0
+ */
+int _putchar(char c)
+{
+	_putchar('c');
+	_putchar('\n');
+}
