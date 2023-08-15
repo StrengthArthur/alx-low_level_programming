febd1d16b4c265542f394da92f4e9e539f97ae9b
StrengthArthur <arthurmillyb55@gmail.com>
2023-08-15 13:21:36 -0700

    Creating a struct template

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+#include <stdio.h>
+/**
+ * struct dog - This is a data type
+ * @name: This is the name of the dog
+ * @age: This is the age of the dog
+ * @owner: This is the owner of the dog
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+#endif
